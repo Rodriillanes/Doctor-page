881fc03d49fa09fd9c0d272da3917a8d6f15cd89
Rodriillanes <rodriillanes@MacBook-Pro-de-Rodrigo.local>
2024-03-15 17:58:15 -0400

    facebookicon

--- doctor-page/src/App.js
+++ doctor-page/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from '@mui/material/Button';
+import Header from './components/header/Header.component'
 
 function App() {
   return (
     <div className="App">
-      <Button variant="contained">Hello world</Button>
+      <Header></Header>
     </div>
   );
 }
