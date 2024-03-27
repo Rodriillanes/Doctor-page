a9a930f01552e9e3e27e56234bb6010add2157a1
Rodriillanes <rodriillanes@MacBook-Pro-de-Rodrigo.local>
2024-03-27 11:36:07 -0400

    Footer

--- doctor-page/src/components/Footer/Footer.component.jsx
+++ doctor-page/src/components/Footer/Footer.component.jsx
@@ -1,6 +1,10 @@
 import { Box, Typography } from '@mui/material'
 import React from 'react'
 
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
 const Footer = () => {
     return(
         <footer>
@@ -8,19 +12,64 @@ const Footer = () => {
                 textAlign: 'center',
                 padding: '1rem',
                 lineHeight: '3 rem',
-                backgroundColor: '#333'
+                backgroundColor: '#333',
+                color: 'white'
             }}>
                 <Typography sx={{fontWeight: 'bold', fontSize: '1.5rem'}}>
                     Corcho
                 </Typography>
                 <Typography>
-
+                    &copy; {new Date().getFullYear()} Maria Canda. All rights reserve
                 </Typography>
                 <Typography>
-
+                    123 calle principal, Ciudad, Estado, País
                 </Typography>
                 <Typography>
-
+                    +591 12345678
+                </Typography>
+                <Typography>
+                    info@example.com
+                </Typography>
+                <Box>
+                    <a href="https://www.instagram.com/" target='blank' rel='noopener noreffer'>
+                        <InstagramIcon 
+                        sx={{
+                            marginRight: '10px',
+                            color: 'white',
+                            transition: 'color 0.3s easy',
+                            '&:hover':{
+                                color: 'rgb(0,0,255)'
+                            },
+                        }}/>
+                    </a>
+                    <a href="https://www.facebook.com/" target='blank' rel='noopener noreffer'>
+                        <FacebookIcon 
+                        sx={{
+                            marginRight: '10px',
+                            color: 'white',
+                            transition: 'color 0.3s easy',
+                            '&:hover':{
+                                color: 'rgb(0,0,255)'
+                            },
+                        }}/>
+                    </a>
+                    <a href="https://www.linkedin.com/" target='blank' rel='noopener noreffer'>
+                        <LinkedInIcon 
+                        sx={{
+                            marginRight: '10px',
+                            color: 'white',
+                            transition: 'color 0.3s easy',
+                            '&:hover':{
+                                color: 'rgb(0,0,255)'
+                            },
+                        }}/>
+                    </a>
+                </Box>
+                <Typography
+                sx={{
+                    fontStyle: 'italic'
+                }}>
+                    Powered by Rodriillanes
                 </Typography>
             </Box>
         </footer>
